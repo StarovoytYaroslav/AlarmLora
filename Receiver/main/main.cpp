07fbdc75c7b153bcb70eac8a709e6bc946cbdc92
y.starovoyt <y.starovoyt@devcaz.com>
2025-11-18 19:59:27 +0200

    working on

---
+++ Receiver/main/main.cpp
@@ -0,0 +1,73 @@
+/*
+ * SPDX-FileCopyrightText: 2010-2022 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: CC0-1.0
+ */
+
+#include <stdio.h>
+#include <inttypes.h>
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_chip_info.h"
+#include "esp_flash.h"
+#include "esp_system.h"
+#include "Arduino.h"
+#include "RadioLib.h"
+#include "SPI.h"
+
+
+#define LORA_CS 8
+#define LORA_DIO1 14
+#define LORA_RST 12
+#define LORA_BUSY 13
+// SPI Pins
+#define LORA_SCK 9
+#define LORA_MISO 11
+#define LORA_MOSI 10
+
+SPIClass loraSPI(1);
+
+// 1. Create settings (2MHz, MSB First, Mode 0 is standard for SX1262)
+SPISettings loraSettings(2000000, MSBFIRST, SPI_MODE0);
+
+// 2. Pass settings explicitly to the constructor
+SX1262 radio = new Module(LORA_CS, LORA_DIO1, LORA_RST, LORA_BUSY, loraSPI, loraSettings);
+
+extern "C" void app_main()
+{
+    // printf("Hello world!\n");
+
+    // /* Print chip information */
+    // esp_chip_info_t chip_info;
+    // uint32_t flash_size;
+    // esp_chip_info(&chip_info);
+    // printf("This is %s chip with %d CPU core(s), %s%s%s%s, ",
+    //        CONFIG_IDF_TARGET,
+    //        chip_info.cores,
+    //        (chip_info.features & CHIP_FEATURE_WIFI_BGN) ? "WiFi/" : "",
+    //        (chip_info.features & CHIP_FEATURE_BT) ? "BT" : "",
+    //        (chip_info.features & CHIP_FEATURE_BLE) ? "BLE" : "",
+    //        (chip_info.features & CHIP_FEATURE_IEEE802154) ? ", 802.15.4 (Zigbee/Thread)" : "");
+
+    // unsigned major_rev = chip_info.revision / 100;
+    // unsigned minor_rev = chip_info.revision % 100;
+    // printf("silicon revision v%d.%d, ", major_rev, minor_rev);
+    // if(esp_flash_get_size(NULL, &flash_size) != ESP_OK) {
+    //     printf("Get flash size failed");
+    //     return;
+    // }
+
+    // printf("%" PRIu32 "MB %s flash\n", flash_size / (uint32_t)(1024 * 1024),
+    //        (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
+
+    // printf("Minimum free heap size: %" PRIu32 " bytes\n", esp_get_minimum_free_heap_size());
+
+    // for (int i = 10; i >= 0; i--) {
+    //     printf("Restarting in %d seconds...\n", i);
+    //     vTaskDelay(1000 / portTICK_PERIOD_MS);
+    // }
+    // printf("Restarting now.\n");
+    // fflush(stdout);
+    // esp_restart();
+}
